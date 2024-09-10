635b22909d6163e668fbf1f20c85cd451cc3eb45
Max Tremaine <max.tremaine@gmail.com>
2024-09-10 13:56:59 -0400

    Refactor

--- WAF24/__main__.py
+++ WAF24/__main__.py
@@ -1,22 +1,18 @@
 from bs4 import BeautifulSoup as bs
 
-base_url = "https://app.aviation-festival.com"
-
 with open("WAF24Attendees.html", "r") as f:
     html = f.read()
 
 soup = bs(html, "html.parser")
 links = soup.find_all("a")
-
-profile_lengths = set()
 profiles = [["Name", "Title", "Company", "Link"]]
+base_url = "https://app.aviation-festival.com"
 
 for link in links:
     spans = link.find_all("span")
     path = link.get("href")
-    profile_lengths.add(len(spans))
 
-    if len(spans) is 5 and len(spans[0].get_text()) is 2:
+    if len(spans) == 5 and len(spans[0].get_text()) == 2:
         profiles.append([
             spans[1].get_text(),
             "",
@@ -24,7 +20,7 @@ for link in links:
             base_url + path
         ])
 
-    if len(spans) is 5 and len(spans[0].get_text()) is not 2:
+    if len(spans) == 5 and len(spans[0].get_text()) != 2:
         profiles.append([
             spans[0].get_text(),
             spans[1].get_text().replace(",", " -"),
@@ -32,7 +28,7 @@ for link in links:
             base_url + path
         ])
 
-    if len(spans) is 6:
+    if len(spans) == 6:
         profiles.append([
             spans[1].get_text(),
             spans[2].get_text().replace(",", " -"),
@@ -40,7 +36,7 @@ for link in links:
             base_url + path
         ])
 
-    if len(spans) is 7:
+    if len(spans) == 7:
         profiles.append([
             spans[2].get_text(),
             spans[3].get_text().replace(",", " -"),
