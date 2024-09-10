d4f3fb5660016d8decc24cc90acd02c59759f902
Max Tremaine <max.tremaine@gmail.com>
2024-09-10 09:52:55 -0400

    Add WAF24

---
+++ WAF24/__main__.py
@@ -0,0 +1,64 @@
+from bs4 import BeautifulSoup as bs
+
+base_url = "https://app.aviation-festival.com"
+
+with open("WAF24Attendees.html", "r") as f:
+    html = f.read()
+
+soup = bs(html, "html.parser")
+links = soup.find_all("a")
+
+profile_lengths = set()
+
+for link in links:
+    spans = link.find_all("span")
+    path = link.get("href")
+    profile_lengths.add(len(spans))
+
+    # print("Profile Length: ", len(spans))
+    # print(f"Link: {base_url}{path}")
+    for span in spans:
+        # print(span.get_text())
+        pass
+
+print(profile_lengths)
+
+# soup = bs(html, 'html.parser')
+# links = soup.find_all('a')
+# profiles = [[ 'airline', 'title', 'name', 'url' ]] # Container for CSV
+
+# for link in links:
+#     content = link.find_all('span')
+
+#     # Escape anything that is not a profile
+#     if len(content) == 0:
+#         continue
+
+#     # The website switches between profile lengths of 3, 4, and 5
+#     if(len(content) == 3):
+#         profile = [
+#             content[2].get_text().replace(',', ' -'),
+#             content[1].get_text().replace(',', ' -'),
+#             content[0].get_text().replace(',', ' -'),
+#             base_url + link.get('href')
+#         ]
+#     if(len(content) == 4):
+#         profile = [
+#             content[3].get_text().replace(',', ' -'),
+#             content[2].get_text().replace(',', ' -'),
+#             content[1].get_text().replace(',', ' -'),
+#             base_url + link.get('href')
+#         ]
+#     if(len(content) == 5):
+#         profile = [
+#             content[4].get_text().replace(',', ' -'),
+#             content[3].get_text().replace(',', ' -'),
+#             content[2].get_text().replace(',', ' -'),
+#             base_url + link.get('href')
+#         ]
+
+#     profiles.append(profile)
+
+# with open('profiles.csv', 'w') as f:
+#     for profile in profiles:
+#         f.write(','.join(profile) + '\n')
